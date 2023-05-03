8647c897b882d600f28e780f3e342e02bd02521a
Iftekher Aziz <azizchy02@gmail.com>
2023-05-03 20:14:07 +0600

    install cors and fecth chef data

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+const chefs = require('./data/chef.json')
 
-
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs);
+});
+
 
 app.listen(port, () => {
   console.log(`the recipe room is running on ${port}`)
