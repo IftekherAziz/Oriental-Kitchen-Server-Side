f9b7cbb1f9348464d25115159eb70b8d01803694
Iftekher Aziz <azizchy02@gmail.com>
2023-05-03 20:14:50 +0600

    create routes for single chef by id

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get('/chefs', (req, res) => {
   res.send(chefs);
 });
 
+app.get('/chefs/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id)
+  const selectedChef = chefs.find(c => c.chef_id === id);
+  res.send(selectedChef);
+})
 
 app.listen(port, () => {
   console.log(`the recipe room is running on ${port}`)
